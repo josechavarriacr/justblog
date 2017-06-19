ba8d489af4a599186f423cd259f2fbf7ff65b462
Jose Chavarria <joserafaelcascante@gmail.com>
2017-06-19 01:57:21 -0600

    clean code

--- frontend/views/post/_category.php
+++ frontend/views/post/_category.php
@@ -46,7 +46,7 @@ $this->title = $modelCategory->name;
 				</div> 
 			</div>
 
-				<div class="col-md-9"><!-- col-md-9 start -->
+			<div class="col-md-9"><!-- col-md-9 start -->
 				<hr>
 				<div class="row">
 					<div class="col-lg-9 col-lg-offset-1">
@@ -58,91 +58,100 @@ $this->title = $modelCategory->name;
 								</h3>
 								<div class="row">
 									<div class="col-lg-3">
-										<p class="text-muted">Visitas <span class="fa fa-eye"> <?=$post['count']?></span></p>
+										<p class="text-muted text-danger"><span class="fa fa-eye"> <?=$post['count']?> visitas </span></p>
 									</div>
 									<div class="col-lg-3-offset-0">
 										<p class="text-muted">Posteado en <?=Yii::$app->formatter->asDate($post['created_at'])?></p>
 									</div>
 								</div>
-								<div class="row">
-									<div class="col-lg-7">
+								<?php $tags = Post::getTagsFront($post['id']);?>
+
+								<?php if ($post['img']): ?>
+									<div class="row">
+										<div class="col-lg-7">
+											<p class="text"><span class="label label-success"><?php 
+												$date=strtotime("-1 Year");
+												if ($post['created_at'] > $date) {
+													echo "nuevo";	
+												} ;?></span> <?=substr($post['descripcion'], 0, 260 ) ?>.</p>
+											</div>	
+
+											<div class="col-lg-offset-5">
+												<?=Html::a(Html::img($post['img'], ['class'=>'img-circle']), [$post['url']]) ?>
+											</div>  
+										</div>
+									<?php else : ?> 
 										<p class="text"><span class="label label-success"><?php 
 											$date=strtotime("-1 Year");
 											if ($post['created_at'] > $date) {
 												echo "nuevo";	
 											} ;?></span> <?=substr($post['descripcion'], 0, 260 ) ?>.</p>
-											<?php $tags = Post::getTagsFront($post['id']);?>
-										</div>	
+										<?php endif; ?> 
 
-										<div class="col-lg-offset-5">
-											<?=Html::a(Html::img($post['img'], ['alt'=>'Cinque Terre', 'class'=>'img-circle']), [$post['url']])?> 
-										</div>  
-									</div>
+										<p><?php foreach($tags as $tag) : ?>
+											<a href="<?= Url::to(['/post/tag/', 'category' => $tag]) ?>" 
+												style="font-size: 16px" class="badge progress-bar-info">#<?=$tag?></a>
+											<?php endforeach; ?></p>
 
-									<p><?php foreach($tags as $tag) : ?>
-										<a href="<?= Url::to(['/post/tag/', 'category' => $tag]) ?>" 
-											style="font-size: 16px" class="badge progress-bar-info">#<?=$tag?></a>
-										<?php endforeach; ?></p>
-
-									</div><hr>
-								<?php endforeach; ?>
-							</div>
-						</div>
-						<hr>
-
-						<?=LinkPager::widget([
-							'pagination' => $pages,
-							]);?> 
-						</div><!-- col-md-9 start -->
-
-						<div class="col-md-3"><!-- div col start -->
-							<hr>
-							<div class="list-group list-group"><!-- div list-group start -->
-								<h4 class="heading">Categorías <i class="glyphicon glyphicon-folder-open"></i></h4>
-								<?php $categories = Post::getCategories();?>
-								<?php foreach ($categories as $cat): ?>
-
-									<a href="<?= Url::to(['post/category/', 'category'=>$cat['url']])?>" class="list-group-item"> 
-										<span style="font-size: 15px" class="badge progress-bar-danger"><?=$cat['count']?></span><?=$cat['name']?></a> 
+										</div><hr>
 									<?php endforeach; ?>
-									<hr class="">	
+								</div>
+							</div>
+							<hr>
 
-									<h4 class="heading">Etiquetas <span class="glyphicon glyphicon-tags"></span></h4>
-									<?php $tags = Post::getTags(); ?>
+							<?=LinkPager::widget([
+								'pagination' => $pages,
+								]);?> 
+							</div><!-- col-md-9 start -->
 
-									<?php foreach (array_slice($tags, 0,5) as $tg): ?>
-										<a href="<?= Url::to(['post/tag/', 'category'=>$tg['name']])?>" class="list-group-item">
-											<span style="font-size: 15px" class="badge progress-bar-danger"><?=$tg['count']?> </span><?=$tg['name']?></a>				
+							<div class="col-md-3"><!-- div col start -->
+								<hr>
+								<div class="list-group list-group"><!-- div list-group start -->
+									<h4 class="heading">Categorías <i class="glyphicon glyphicon-folder-open"></i></h4>
+									<?php $categories = Post::getCategories();?>
+									<?php foreach ($categories as $cat): ?>
+
+										<a href="<?= Url::to(['post/category/', 'category'=>$cat['url']])?>" class="list-group-item"> 
+											<span style="font-size: 15px" class="badge progress-bar-danger"><?=$cat['count']?></span><?=$cat['name']?></a> 
 										<?php endforeach; ?>
+										<hr class="">	
 
-										<div id="categories" class="collapse">
-											<?php foreach (array_slice($tags, 5) as $tg): ?>
-												<a href="<?= Url::to(['post/tag/', 'category'=>$tg['name']])?>" class="list-group-item">
-													<span style="font-size: 15px" class="badge progress-bar-danger"><?=$tg['count']?> </span><?=$tg['name']?></a>				
-												<?php endforeach; ?>
+										<h4 class="heading">Etiquetas <span class="glyphicon glyphicon-tags"></span></h4>
+										<?php $tags = Post::getTags(); ?>
 
-											</div>
+										<?php foreach (array_slice($tags, 0,5) as $tg): ?>
+											<a href="<?= Url::to(['post/tag/', 'category'=>$tg['name']])?>" class="list-group-item">
+												<span style="font-size: 15px" class="badge progress-bar-danger"><?=$tg['count']?> </span><?=$tg['name']?></a>				
+											<?php endforeach; ?>
 
-											<button class="btn btn-default btn-sm btn-block" data-toggle="collapse" data-target="#categories">More <span class="caret"></span></button>
-											<hr class="">
+											<div id="categories" class="collapse">
+												<?php foreach (array_slice($tags, 5) as $tg): ?>
+													<a href="<?= Url::to(['post/tag/', 'category'=>$tg['name']])?>" class="list-group-item">
+														<span style="font-size: 15px" class="badge progress-bar-danger"><?=$tg['count']?> </span><?=$tg['name']?></a>				
+													<?php endforeach; ?>
 
-											<h4 class="heading">Hecho en Costa Rica</h4>
-											<div class="bandera costarica" title="Costa Rica"></div>
-											<hr class="">
+												</div>
+
+												<button class="btn btn-default btn-sm btn-block" data-toggle="collapse" data-target="#categories">More <span class="caret"></span></button>
+												<hr class="">
+
+												<h4 class="heading">Hecho en Costa Rica</h4>
+												<div class="bandera costarica" title="Costa Rica"></div>
+												<hr class="">
 
 
-										</div><!-- div list-group end -->
-									</div><!-- div col end -->
-								</div><!-- dic end row -->
-							</div><!-- div end container -->
-						</div>
+											</div><!-- div list-group end -->
+										</div><!-- div col end -->
+									</div><!-- dic end row -->
+								</div><!-- div end container -->
+							</div>
 
-						<br>
-						<br>
-						<br>
-						<br>
-						<br>
-						<br>
-						<br>
-						<br>
-						<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
