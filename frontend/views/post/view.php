03028524fa9ab5b57cff22906c637c0a4ceb9d12
Jose Chavarria <legendarioj4r@hotmail.com>
2017-07-24 23:49:08 -0600

    without recommended post in post view

--- frontend/views/post/view.php
+++ frontend/views/post/view.php
@@ -45,10 +45,10 @@ Yii::$app->meta->getMetaTags($model->id);
 							'url' => $model->url,
 							]); ?>
 							<!-- end _share -->
-
+							<hr>
 						</div>
 					</article>
-				</div><hr>
+				</div>
 
 				<div class="row">
 					<div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
